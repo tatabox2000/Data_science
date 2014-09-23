9a58e6f41b885d5dcc81102df310ba64bd7be5e4
tatabox2000 <tatabox2000@gmail.com>
2014-09-24 02:19:38 +0900

    coordinate

--- pyside_blog/matrix_co.py
+++ pyside_blog/matrix_co.py
@@ -4,10 +4,15 @@ import cv2
 
 class coordinateForCv:
 	def __init_(self,parent=None):
-		self.file = file
+		self.pyqt_pic = None
 		self._x = None
 		self._y = None
-	def cv2qtgraph(self,im=None):
+
+	def cv2open(self,file=None):
+		im = cv2.imread(file)
+		return im
+
+	def cv2pyqtgraph(self,im=None):
 		if im.shape[2] == 3:
 			im_c = cv2.cvtColor(im,cv2.COLOR_BGR2RGB)
 			im = im_c
@@ -15,31 +20,35 @@ class coordinateForCv:
 			pass
 		rows,cols,dim = im.shape
 		M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
-		dst = cv2.warpAffine(im,M,(cols,rows))
-		return dst
+		self.pyqt_pic = cv2.warpAffine(im,M,(cols,rows))
+		return self.pyqt_pic
+
+	def coordinate2cv(self,x=None,y=None):
+		temp_coor =  np.zeros((self.pyqt_pic.shape[0],self.pyqt_pic.shape[1]),np.uint8)
+		index = zip(x,y)
+		for i,j in index:
+			#print j,i
+			temp_coor[j:j+3,i] = 255
+		rows,cols = temp_coor.shape
+		M = cv2.getRotationMatrix2D((cols/2,rows/2),-90,1)
+		cv_pic_coor = cv2.warpAffine(temp_coor,M,(cols,rows))
+		index2 = np.where(cv_pic_coor==255)
+		cv_coor = zip(index2[0],index2[1])
+		return temp_coor,cv_pic_coor,cv_coor
 
 if __name__ == '__main__':
 	file = "lena.jpg"
-	im = cv2.imread(file)
-        a = coordinateForCv()
-	pic = a.cv2qtgraph(im)
+	a = coordinateForCv()
+	im = a.cv2open(file)
+	im2 = a.cv2pyqtgraph(im)
+	x = np.arange(10,100,1)
+	y = np.arange(30,130,1)
+	before,after,calc = a.coordinate2cv(x,y)
+	calc_pic = np.zeros((im2.shape[0],im2.shape[1]),np.uint8)
 
-	test = np.zeros((pic.shape[0],pic.shape[1]),np.uint8)
-	test2 = np.zeros((pic.shape[0],pic.shape[1]),np.uint8)
-	test[5:8,10:30] = 255
-	index = np.where(test==255)
-	rows,cols = test.shape
-	M = cv2.getRotationMatrix2D((cols/2,rows/2),-90,1)
-	dst = cv2.warpAffine(test,M,(cols,rows))
-	index2 = np.where(dst==255)
-
-	coor = zip(index2[0],index2[1])
-	for i,j in coor:
-		test2[i,j]=254
-	print coor
-	#print index
-	#print index2
-	plt.subplot(1,3,1),plt.imshow(test,'gray')
-	plt.subplot(1,3,2),plt.imshow(dst,'gray')
-	plt.subplot(1,3,3),plt.imshow(test2,'gray')
+	for i,j in calc:
+		calc_pic[i,j]=255
+	plt.subplot(1,3,1),plt.imshow(before,'gray')
+	plt.subplot(1,3,2),plt.imshow(after,'gray')
+	plt.subplot(1,3,3),plt.imshow(calc_pic,'gray')
 	plt.show()
